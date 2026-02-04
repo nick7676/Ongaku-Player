3859eafad702903a1ff07b6f26820506dd86f16b
nicolò vaccaro <nicolo.vaccaro@appfactory.it>
2026-02-04 16:08:32 +0100

    Changed how yt-dlp is bundled and fixed the old version

--- src-tauri/src/functions/mod.rs
+++ src-tauri/src/functions/mod.rs
@@ -1,2 +1,3 @@
 pub mod start;
 pub mod user_info;
+pub mod utils;
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,17 +2,23 @@ mod functions;
 
 use functions::start::check_download_folder;
 use functions::user_info::get_username;
+use functions::utils::check_ytdlp_backend;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
-        .setup(|_app| {
+        .plugin(tauri_plugin_shell::init())
+        .setup(|app| {
             check_download_folder();
+            let handle = app.handle().clone();
+            tauri::async_runtime::spawn(async move {
+                check_ytdlp_backend(handle).await;
+            });
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![get_username])
+        .invoke_handler(tauri::generate_handler![get_username, check_ytdlp_backend])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
